677b12703ef87ec5d277fbf4aae3cf04c4e2e9a5
NilouJ <n.jamshidi@alumni.ut.ac.ir>
2024-11-06 12:05:52 +1100

    flye assembly added subprocess

--- python_scr/2- Flye.py
+++ python_scr/2- Flye.py
@@ -1,9 +1,9 @@
 import subprocess
 import os
+import glob
 
 
 def run_flye(fastq_dir, output_dir, genome_size, threads=6):
-    # Verify directories exist
     if not os.path.isdir(fastq_dir):
         print(f"Error: The specified fastq directory '{fastq_dir}' does not exist.")
         return
@@ -12,16 +12,20 @@ def run_flye(fastq_dir, output_dir, genome_size, threads=6):
         os.makedirs(output_dir)
         print(f"Output directory '{output_dir}' created.")
 
-    # Construct the Flye command
-    flye_cmd = [
-        "flye",
-        "--nano-raw", os.path.join(fastq_dir, "*.fastq.gz"),
-        "--out-dir", output_dir,
-        "--genome-size", genome_size,
-        "--threads", str(threads)
-    ]
+    fastq_files = glob.glob(os.path.join(fastq_dir, "*.fastq.gz"))
+    if not fastq_files:
+        print(f"Error: No .fastq.gz files found in the specified directory '{fastq_dir}'.")
+        return
 
-    # Run the Flye command
+    flye_cmd = [
+                   "flye",
+                   "--nano-raw"
+               ] + fastq_files + [
+                   "--out-dir", output_dir,
+                   "--genome-size", genome_size,
+                   "--threads", str(threads)
+               ]
+
     try:
         subprocess.run(flye_cmd, check=True)
         print("Flye assembly completed successfully.")
@@ -31,13 +35,10 @@ def run_flye(fastq_dir, output_dir, genome_size, threads=6):
         print("Error: Flye is not installed or not found in PATH.")
 
 
-# Example usage:
-# Customize these variables with your paths and parameters
 fastq_directory = ("/Users/MQ10005295/Library/CloudStorage/OneDrive-MacquarieUniversity/Nilou/Nanopore/ONTseq_Data"
-                   "/basecalling/pass/barcode01")
-output_directory = ("/Users/MQ10005295/Library/CloudStorage/OneDrive-MacquarieUniversity/Nilou/Nanopore/result_flye/5k"
-                    "/barcode01")
-genome_size = "6.25k"
+                   "/basecalling/pass/barcode03")
+output_directory = ("/Users/MQ10005295/Library/CloudStorage/OneDrive-MacquarieUniversity/Nilou/Nanopore/result_flye/6k"
+                    "/barcode03")
+genome_size = "6k"
 
-# Run the function
 run_flye(fastq_directory, output_directory, genome_size)
